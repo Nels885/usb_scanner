dd71f3a18140f8accbdc0ae1a1d9abc677596e0e
nels885 <nels885@protonmail.com>
2022-03-06 18:57:53 +0100

    Added arguments to Reader class and changed filename for list of devices

--- usb_scanner/usb_devices.py
+++ usb_scanner/usb_devices.py
@@ -1,6 +1,6 @@
 scanners = [
-    # Name, idVendor, idProduct, chunk_size
-    ('Motorola', 0x05e0, 0x1200, 16),
+    # deviceName, idVendor, idProduct, chunkSize
+    ('Motorola ls1203', 0x05e0, 0x1200, 16),
     ('Datalogic', 0x080c, 0x0300, 16),
     ('Metrologic Instruments 1470g', 0x0c2e, 0x1001, 8),
 ]
